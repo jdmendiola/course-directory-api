594f80016656fb1373e922aa45393e324223f5c8
jdmendiola <jdmmendiola@gmail.com>
2019-04-10 01:38:55 -0400

    Added email validation to user model

--- src/models/User.js
+++ src/models/User.js
@@ -11,7 +11,14 @@ let UserSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        trim: true
+        trim: true,
+        validate: {
+            validator: function (value) {
+                let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+                return emailRegex.test(value);
+            },
+            message: 'Not a valid email format'
+        }
     },
     password: {
         type: String,
