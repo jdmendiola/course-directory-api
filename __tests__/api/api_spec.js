f0c7ad47a1c7fc4e579d56eea5359107a18f29dc
jdmendiola <jdmmendiola@gmail.com>
2019-05-07 01:23:14 -0400

    Added unit test for :courseID route

--- __tests__/api/api_spec.js
+++ __tests__/api/api_spec.js
@@ -12,3 +12,16 @@ it ('When I make a request to the GET route with the correct credentials, the co
     .get('http://localhost:5000/api/users')
     .expect('status', 200);
 });
+
+it ('When I make a request to the GET /api/courses/:courseId route with the invalid credentials, a 401 status error is returned', function () {
+    return frisby
+    .setup({
+        request: {
+            headers: {
+                'Authorization': 'Basic ' + Buffer.from("jdm@jdm.com:vbnmm").toString('base64')
+            }
+        }
+    })
+    .get('http://localhost:5000/api/courses/57029ed4795118be119cc43d')
+    .expect('status', 401);
+});
